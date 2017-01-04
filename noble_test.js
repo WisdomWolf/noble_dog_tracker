4973eb48ef8c2095b0aead93c625c394989fe3f0
WisdomWolf <wisdomwolf@gmail.com>
2017-01-04 16:55:10 -0500

    added new logger - winston

--- noble_test.js
+++ noble_test.js
@@ -1,8 +1,9 @@
 var noble = require('noble');
 var http = require('http');
 const mqtt = require('mqtt');
+var winston = require('winston');
 const client = mqtt.connect('mqtt://192.168.1.22');
-var seenCount = 0;
+winston.add(winston.transports.File, { filename: 'noble_test.log'});
 
 var options = {
   host: 'nodered-wisehub.pagekite.me',
@@ -19,12 +20,12 @@ callback = function(response) {
 
   //errors
   response.on('error', function (e) {
-    console.log(e);
+    winston.error('Callback Error: ' + e);
   });
 
   //the whole response has been recieved, so we just print it out here
   response.on('end', function () {
-    console.log(str);
+    winston.info(str);
   });
 }
 
@@ -32,15 +33,16 @@ callback = function(response) {
 noble.on('stateChange', function(state) {
   if (state === 'poweredOn') {
     noble.startScanning([], true);
-    console.log('Noble scanner started...');
+    winston.log('Noble scanner started...');
   }  else  {
     noble.stopScanning();
-    console.log('Noble scanner stopped.');
+    winston.log('Noble scanner stopped.');
   }
 });
 
 var rikou = /960c.*?60077ad/i;
 var testUUID = 'fefd';
+var recentlySeen = {};
 
 noble.on('discover', function(peripheral) {
     var now = new Date().toLocaleString();
@@ -55,21 +57,21 @@ noble.on('discover', function(peripheral) {
     } else if (serviceUuids == testUUID) {
       entity = 'test';
     }
-    if (entity && rssi > -90) {
-      console.log(now, ' - found: ', entity, ' ', rssi);
-      if (rssi > -80) {
+    if (entity && rssi > -80) {
+      winston.log(now, ' - found: ', entity, ' ', rssi);
+      if (rssi > -75) {
         //client.publish('dogs/rikou', rssi);
-        seenCount++;
+        recentlySeen[entity]++;
       } else {
-        seenCount = 0;
+        recentlySeen[entity] = 0;
       }
-      if (seenCount > 2) {
+      if (recentlySeen[entity] > 2) {
         var req = http.request(options, callback);
-        req.on('error', function(e) {
-          console.log(e);
-        });
         req.end();
-        console.log('!!!http alert sent !!!');
+        req.on('error', function(e) {
+          winston.error(e);
+        });
+        winston.log('!!!http alert sent !!!');
       }
       //console.log('Manufacturer Data: ', manufacturerData);
       // console.log('---');
