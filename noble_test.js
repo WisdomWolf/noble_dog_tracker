c6607664d58534dc73ec0ca7d37ad6e723181bc2
WisdomWolf <wisdomwolf@gmail.com>
2017-01-07 22:51:14 -0500

    removed redundant now vars

--- noble_test.js
+++ noble_test.js
@@ -5,8 +5,14 @@ var app = express();
 const mqtt = require('mqtt');
 var winston = require('winston');
 const client = mqtt.connect('mqtt://192.168.1.22');
-winston.add(winston.transports.File, { filename: 'noble_test.log'});
-winston.level = 'debug';
+const logger = new (winston.Logger)({
+  transports: [
+    // colorize the output to the console
+    new (winston.transports.Console)({ colorize: true }),
+    new (winston.transports.File)({ filename: 'noble_test.log'})
+  ]
+});
+logger.level = 'debug';
 
 var options = {
   host: 'nodered-wisehub.pagekite.me',
@@ -23,12 +29,12 @@ callback = function(response) {
 
   //errors
   response.on('error', function (e) {
-    winston.error('Callback Error: ' + e);
+    logger.error('Callback Error: ' + e);
   });
 
   //the whole response has been recieved, so we just print it out here
   response.on('end', function () {
-    winston.info(str);
+    logger.info(str);
   });
 }
 
@@ -36,11 +42,18 @@ app.get('/', function(req, res) {
   res.send('Hello World');
 });
 
+app.get('/log', function(req, res) {
+  var entity = req.query.entity;
+  var location = req.query.location;
+  var locStatus = entity + ' @ ' + location;
+  logger.debug(locStatus);
+  res.send('Recording to log: ' + locStatus);
+});
+
 app.get('/rikou', function(req, res) {
-  var now = new Date().toLocaleString();
   var location = req.query.in;
-  var dogStatus = now + ' - Rikou is in ' + location;
-  winston.debug(dogStatus);
+  var dogStatus = 'Rikou is in ' + location;
+  logger.debug(dogStatus);
   res.send('Logging: ' + dogStatus);
 });
 
@@ -48,16 +61,16 @@ var server = app.listen(8081, function() {
   var host = server.address().address
   var port = server.address().port
 
-  winston.debug("Example app listening at http://%s:%s", host, port);
+  logger.debug("Dog Tracker app listening at http://%s:%s", host, port);
 });
 
 noble.on('stateChange', function(state) {
   if (state === 'poweredOn') {
     noble.startScanning([], true);
-    winston.info('Noble scanner started...');
+    logger.info('Noble scanner started...');
   }  else  {
     noble.stopScanning();
-    winston.info('Noble scanner stopped.');
+    logger.info('Noble scanner stopped.');
   }
 });
 
@@ -66,7 +79,6 @@ var testUUID = 'fefd';
 var recentlySeen = {};
 
 noble.on('discover', function(peripheral) {
-    var now = new Date().toLocaleString();
     var macAddress = peripheral.uuid;
     var rssi = peripheral.rssi;
     var localName = peripheral.advertisement.localName;
@@ -82,7 +94,7 @@ noble.on('discover', function(peripheral) {
       recentlySeen[entity] = 0;
     }
     if (entity && rssi > -80) {
-      winston.verbose(now, ' - found: ', entity, ' ', rssi);
+      logger.verbose('found: ', entity, ' ', rssi);
       if (rssi > -75) {
         //client.publish('dogs/rikou', rssi);
         recentlySeen[entity]++;
@@ -93,9 +105,9 @@ noble.on('discover', function(peripheral) {
         var req = http.request(options, callback);
         req.end();
         req.on('error', function(e) {
-          winston.error(e);
+          logger.error('Error in HTTP Request - ' + e);
         });
-        winston.warn(now, '!!!http alert sent !!!');
+        logger.warn(now, '!!!http alert sent !!!');
       }
     }
 });
